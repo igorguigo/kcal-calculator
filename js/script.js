21b1a283e3ed82a6104a61f110277e851875dffd
Igor Tonelli <igorgodoy2010@hotmail.com>
2023-06-18 23:59:07 -0300

    master | Calculadora de calorias completa com indicador de quantidade de gramas de cada macro nutriente

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
-let sexo = document.querySelector("input[name='sexo']");
+let sexo = document.querySelectorAll("input[name='sexo']");
 let peso = document.querySelector("input[name='peso']");
 let altura = document.querySelector("input[name='altura']");
 let idade = document.querySelector("input[name='idade']");
 let nv_atividade = document.querySelector("select[name='nv-atividade']");
-let btn = document.querySelector(".btn");
+let objetivo = document.querySelector("select[name='objetivo']");
 let form = document.querySelector("form");
-let radioGroup = document.querySelectorAll("input[type='radio'][name='sexo']");
-let selecionado = false;
+
+let kcalData = {
+  totalKcal: 0,
+  proteinKcal: 0,
+  carboKcal: 0,
+  fatKcal: 0,
+}
+
 
 // Taxa de atividade:
 // Sedentários (pouco ou nenhum exercício) fator = 1.2
@@ -17,41 +23,53 @@ let selecionado = false;
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    sexoValue = sexo.value;
-    pesoValue = peso.value;
-    alturaValue = altura.value;
-    idadeValue = idade.value;
-    nv_atividadeValue = nv_atividade.value;
+    //let sexoValue = sexo.value;
+    let pesoValue = parseFloat(peso.value.replace(',', '.'));
+    let alturaValue = altura.value;
+    let idadeValue = idade.value;
+    let nv_atividadeValue = nv_atividade.value;
+    let objetivoValue = objetivo.value;
+    let sexoValue = '';
 
-    // for (var i = 0; i < radioGroup.length; i++) {
-    //   if (radioGroup[i].checked) {
-    //     selecionado = true;
-    //     break;
-    //   }
-    // }
+    for (let i = 0; i < sexo.length; i++) {
+      if (sexo[i].checked) {
+        sexoValue = sexo[i].value;
+        break;
+      }
+    }
+    
 
-    // if(selecionado == false){
-    //   alert('Preencha o campo Sexo');
-    //   return;
-    // }else if(pesoValue == ''){
-    //   alert('Preencha o campo Peso')
-    //   return;
-    // }
-
-    console.log(nv_atividadeValue);
-
-    if(sexoValue == "masculino"){
-        fatorAtividade = calcularNvAtividade(nv_atividadeValue);
-        tmbHomens = fatorAtividade * (66 + ((13.7 * pesoValue) + (5 * alturaValue) - (6.8 * idadeValue)));
+    if(sexoValue === "masculino"){
+        let fatorAtividadeHomens = calcularNvAtividade(nv_atividadeValue);
+        let tmbHomens = fatorAtividadeHomens * (66 + (13.7 * pesoValue) + (5 * alturaValue) - (6.8 * idadeValue));
         console.log(tmbHomens);
+        let totalKcalHomens = calcularTotalKcal(tmbHomens, objetivoValue);
+        console.log(totalKcalHomens);
+        document.querySelector('h2').classList.remove('hidden');
+        document.querySelector('h3').classList.remove('hidden');
+        document.querySelector('.total-calorias').innerHTML =  parseInt(totalKcalHomens.totalKcal.toFixed(0)) + ' kcal';
+        document.querySelector('.total-protein').classList.remove('hidden');
+        document.querySelector('.total-protein span').innerHTML = parseInt(totalKcalHomens.proteinKcal.toFixed(0)) + '  g';
+        document.querySelector('.total-carbo').classList.remove('hidden');
+        document.querySelector('.total-carbo span').innerHTML = parseInt(totalKcalHomens.carboKcal.toFixed(0)) + '  g';
+        document.querySelector('.total-fat').classList.remove('hidden');
+        document.querySelector('.total-fat span').innerHTML = parseInt(totalKcalHomens.fatKcal.toFixed(0)) + '  g';
 
-    }else if(sexoValue == "feminino"){
-        fatorAtividade = calcularNvAtividade(nv_atividadeValue);
-        tmbMulheres = fatorAtividade * (655 + ((9.6 * pesoValue) + (1.8 * alturaValue) - (4.7 * idadeValue)));
+    }else if(sexoValue === "feminino"){
+        let fatorAtividadeMulheres = calcularNvAtividade(nv_atividadeValue);
+        let tmbMulheres = fatorAtividadeMulheres * (655 + (9.6 * pesoValue) + (1.8 * alturaValue) - (4.7 * idadeValue));
         console.log(tmbMulheres);
-
-    }else{
-
+        let totalKcalMulheres = calcularTotalKcal(tmbMulheres, objetivoValue);
+        console.log(totalKcalMulheres);
+        document.querySelector('h2').classList.remove('hidden');
+        document.querySelector('h3').classList.remove('hidden');
+        document.querySelector('.total-calorias').innerHTML =  parseInt(totalKcalMulheres.totalKcal.toFixed(0)) + ' kcal';
+        document.querySelector('.total-protein').classList.remove('hidden');
+        document.querySelector('.total-protein span').innerHTML = parseInt(totalKcalMulheres.proteinKcal.toFixed(0)) + '  g';
+        document.querySelector('.total-carbo').classList.remove('hidden');
+        document.querySelector('.total-carbo span').innerHTML = parseInt(totalKcalMulheres.carboKcal.toFixed(0)) + '  g';
+        document.querySelector('.total-fat').classList.remove('hidden');
+        document.querySelector('.total-fat span').innerHTML = parseInt(totalKcalMulheres.fatKcal.toFixed(0)) + '  g';
     }
 
     // Fórmula para homens: TMB = fator da taxa de atividade x {66 + [(13,7 x Peso(kg)) + ( 5 x Altura(cm)) – (6,8 x Idade(anos))]}
@@ -77,6 +95,32 @@ function calcularNvAtividade(nv_atividade){
     return 1.9;
     break;  
 }
+}
+
+function calcularTotalKcal(tmb, objetivo){
+  switch(objetivo){
+    case "Emagrecer":
+      kcalData.totalKcal = tmb - 500;
+      kcalData.proteinKcal = kcalData.totalKcal * 0.35 / 4;
+      kcalData.carboKcal = kcalData.totalKcal * 0.4 / 4;
+      kcalData.fatKcal = kcalData.totalKcal * 0.25 / 9;
+      return kcalData;
+      break;
+    case "Manter o peso":
+      kcalData.totalKcal = tmb;
+      kcalData.proteinKcal = kcalData.totalKcal * 0.25 / 4;
+      kcalData.carboKcal = kcalData.totalKcal * 0.45 / 4;
+      kcalData.fatKcal = kcalData.totalKcal * 0.30 / 9;
+      return kcalData;
+      break;
+    case "Ganhar peso(Massa muscular)":
+      kcalData.totalKcal = tmb + 500;
+      kcalData.proteinKcal = kcalData.totalKcal * 0.30 / 4;
+      kcalData.carboKcal = kcalData.totalKcal * 0.5 / 4;
+      kcalData.fatKcal = kcalData.totalKcal * 0.2 / 9;
+      return kcalData;
+      break;
+  }
 }
 
 function inputHandler(masks, max, event) {
